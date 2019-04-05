5f51f7fb04dec6cea02d2b019db2e63d421deb74
Ben Chartrand <ben@spottersystems.com>
2019-04-06 10:29:46 +1300

    Latest improvements

--- test/week.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import week from '~/components/week.vue'
-
-// describe('Week component', () => {
-//   test('is a Vue instance', () => {
-//     const wrapper = mount(week)
-//     expect(wrapper.isVueInstance()).toBeTruthy()
-//   })
-// })
